27c8f0c37bf3e6c611e913c6f09396db46b17d5e
a5vTech <a5vgaminghq@gmail.com>
2019-06-23 20:44:26 +0200

    fix register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {MatSnackBar} from "@angular/material";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -9,7 +11,7 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private snackBar: MatSnackBar, private router: Router) {
   }
 
   ngOnInit() {
@@ -19,9 +21,26 @@ export class RegisterComponent implements OnInit {
       email: ['', Validators.required],
       address: ['', Validators.required],
       phonenumber: ['', Validators.required],
-      password: ['', Validators.required, Validators.minLength(6)],
+      password: ['', [Validators.required, Validators.minLength(6)]],
 
     });
+
+  }
+
+
+  register(): void {
+    if (this.registerForm.valid) {
+      this.snackBar.open('Welcome, ' +
+        this.registerForm.controls.firstname.value + ' ' + this.registerForm.controls.lastname.value + '. You may now login!', 'Close', {
+        duration: 2000,
+      });
+
+      this.router.navigate(['/home/login'])
+    } else {
+      this.snackBar.open('Please fill in all the fields', 'Close', {
+        duration: 2000,
+      });
+    }
 
   }
 
